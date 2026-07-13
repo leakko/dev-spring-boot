d319e4731d8774e3d1564dbc5d7c06c611b92d0c
MARCOS RODRIGUEZ SALVO <mrodriguezs@caixabanktech.com>
2026-07-13 11:57:30 +0200

    first commit

---
+++ 02-sping-boot-sping-core/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,19 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TennisCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+}
